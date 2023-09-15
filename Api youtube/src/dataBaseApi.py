1483ce05671a3d162f13a26a2dc4253253d0d3d8
Ludwing Hernandez Caro <79872566+Ludwinghc@users.noreply.github.com>
2023-09-15 15:44:42 -0500

    changes

--- Api youtube/src/dataBaseApi.py
+++ Api youtube/src/dataBaseApi.py
@@ -1,10 +1,15 @@
 import mysql.connector
 
 def conexionDatabase():
-  conex = mysql.connector.connect(
-      host="ludwingengine.mysql.pythonanywhere-services.com",
-      user = "ludwingengine",
-      password = "150102Br",
-      database ="ludwingengine$apiYoutube",
-      port ="3306")
-  return conex
+    try:
+        conex = mysql.connector.connect(
+            host="ludwingengine.mysql.pythonanywhere-services.com",
+            user="ludwingengine",
+            password="020916lAU",
+            database="ludwingengine$apiYoutube",
+            port="3306"
+        )
+        return conex
+    except mysql.connector.Error as err:
+        print(f"Error de conexión a la base de datos: {err}")
+        return None
--- Api youtube/youtubeChanel.py
+++ Api youtube/youtubeChanel.py
@@ -1,12 +1,18 @@
+# Este Documento contiene la logica de los end-points de la api de canales de youtube
+# Desarrollado por ludwing.engine y por Bryan.engine
+
 # Inicio de importacion de librerias
 import os
 from functools import wraps
 from flask import Flask, jsonify, request
 import requests
+import re
 from bs4 import BeautifulSoup
 from src import dataBaseApi as dataBase
+# Fin de importacion de librerias
 
-def scrapingYoutubeDescription(nombre_canal):
+# Inicio de la funcion para realizar el web Scrapping a youtube
+def obtener_descripcion_y_url_imagen_canal_youtube(nombre_canal):
     try:
         # Componemos la URL completa del canal
         url_canal = f"https://www.youtube.com/{nombre_canal}"
@@ -14,6 +20,14 @@ def scrapingYoutubeDescription(nombre_canal):
         # Realizamos una solicitud HTTP para obtener el contenido de la página del canal
         response = requests.get(url_canal)
         response.raise_for_status()
+
+        # Extraemos la URL de la imagen de perfil del canal directamente del código fuente
+        match = re.search(r'"avatar":{"thumbnails":\[{"url":"(https:\/\/[^"]+)","width":', response.text)
+        
+        if match:
+            url_imagen_perfil = match.group(1)
+        else:
+            url_imagen_perfil = "No se encontró una imagen de perfil para este canal."
 
         # Analizamos el contenido HTML de la página
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -24,42 +38,58 @@ def scrapingYoutubeDescription(nombre_canal):
         if meta_description:
             # Extraemos el contenido de la descripción del canal
             descripcion = meta_description['content'].strip()
-            return descripcion
         else:
-            return "No se encontró una descripción para este canal."
+            descripcion = "No se encontró una descripción para este canal."
 
+        return descripcion, url_imagen_perfil
     except Exception as e:
-        return str(e)
+        return str(e), "No se pudo obtener la descripción ni la URL de la imagen."
 
+# Fin de la funcion para el web Scrapping en youtube
 
+# Declaracion del nombre de como se van a manejar los endpoints de la api
 api = Flask(__name__)
 
-
+# Inicio de la 
+# Definicion del primer endpoint para la api el cual solo tiene la funcion de mostrar que la api esta activa
 @api.route('/')
 def index():
     return jsonify({
-      "Esta es la api de canales de youtube": "Bienvenido a nuestra api"
+      "Esta es la api de canales de youtube": "Bienvenido a nuestra api" 
     })
-print(dataBase.conexionDatabase)
+# Fin de la 
+# Definicion del primer endpoint para la api el cual solo tiene la funcion de mostrar que la api esta activa
 
+# Inicio de la declaracion de
+# Definicion del segundo endpoint para la api donde se genera el codigo para guardar la informacion en la base de datos 
 @api.route('/consult', methods=['POST'])
 def consult():
     try:
+      # definimos una variable en la cual se albergara el nombre del canal que los usuarios busquen
       nameChannel = request.form['channel_name'] 
-      print(nameChannel)
-      descriptionChannel = scrapingYoutubeDescription(nombre_canal=nameChannel)
+      # Enviamos a la funcion que creamos para el scrapping
+      # y guardamos en una variable la descripción que retorna la funcion
+      descriptionChannel, imagenChannel = obtener_descripcion_y_url_imagen_canal_youtube(nombre_canal=nameChannel)
       print(descriptionChannel)
+      print(imagenChannel)
+      # Realizamos el llamado para realizar la conexion a la base de datos
       newConex = dataBase.conexionDatabase()
       print(newConex)
       cursor = newConex.cursor()
-      sentencia = "INSERT INTO History (nameChannel, description) VALUES ('"+nameChannel+"', '"+descriptionChannel+"');"
-      cursor.execute(sentencia)
+      sentencia = "INSERT INTO History (nameChannel, description, imagenChannel) VALUES ($s, %s, %s);"
+      cursor.execute(sentencia,(nameChannel, descriptionChannel, imagenChannel))
+
+      return jsonify({
+          "Canal" : nameChannel,
+          "description" : descriptionChannel,
+          "imagenChannel" : imagenChannel
+      })
     except Exception as e:
         return jsonify({
             "Message": "Error. "+ str(e) 
-        })
-    
-
+        })    
+# Inicio de la declaracion de
+# Definicion del segundo endpoint para la api donde se genera el codigo para guardar la informacion en la base de datos 
 
 
 if __name__ == '__main__':
